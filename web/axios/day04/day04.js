d61e9aa478c683b78261b54c4c9be4b26e6c33a4
anthropologistbiubiu <1319847957@qq.com>
2023-10-30 00:16:07 +0800

    modify

--- web/axios/day04/day04.js
+++ web/axios/day04/day04.js
@@ -1,3 +1,10 @@
 
 
-const wait = msg => new Promise((resolve,reject) => setTimeout(resolve,ms));
+//const wait = msg => new Promise((resolve,reject) => setTimeout(resolve,ms));
+
+
+console.log("开始执行");
+setTimeout(function{
+    console.log("延迟执行");
+}, 2000); // 在2秒后执行
+console.log("继续执行");
