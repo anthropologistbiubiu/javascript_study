51359cab52b855c6252f14f9ad63411dd0cf8032
anthropologistbiubiu <1319847957@qq.com>
2023-11-05 14:10:02 +0800

    modify

--- basic/day01/day01.js
+++ basic/day01/day01.js
@@ -1,1 +1,18 @@
 // 循环语句
+
+//1. for 循环
+
+var step
+
+for(step=0;step<10;step++) {
+    console.log('step',step)
+}
+//2.do while 循环
+
+var i = 0
+do{
+    i+=1;
+    console.log('i',i);
+}while(i<5);
+
+
