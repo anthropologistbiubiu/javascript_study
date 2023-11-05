5727a697352fcf8fe62cbb45fdd9ba51cf48177d
anthropologistbiubiu <1319847957@qq.com>
2023-11-05 16:52:46 +0800

    modify

---
+++ basic/day10/day10.js
@@ -0,0 +1,32 @@
+// 数字与日期
+
+const biggestNum = Number.MAX_VALUE
+console.log('biggestNum',biggestNum)
+
+const smallestNum = Number.MIN_VALUE
+console.log('smallestNum',smallestNum)
+
+
+const infiniteNum = Number.POSITIVE_INFINITY
+const  notNum = Number.NaN
+
+const negInfiniteNum = Number.NEGATIVE_INFINITY
+console.log('negInfiniteNum',negInfiniteNum)
+
+
+
+var number1 = Number.parseFloat('123456.1111')
+var number2 = Number.parseInt('123456.222')
+console.log('number1',number1)
+console.log('number2',number2)
+
+
+
+
+
+
+
+
+
+
+
