23c8e46460527a91178449df837c41ce34efd26d
anthropologistbiubiu <1319847957@qq.com>
2023-11-07 00:29:21 +0800

    modify

--- basic/day11/day11.js
+++ basic/day11/day11.js
@@ -1,1 +1,33 @@
 // map 的用法
+
+const saying = new Map()
+
+
+saying.set('dog','woof');
+saying.set('cat','meow');
+saying.set('elephant','toot');
+saying.set('dog','woof');
+console.log(saying.size)
+console.log(saying.get('cat'))
+
+for (const [key,value] of saying) {
+    console.log(key,value);
+};
+
+saying.clear()
+console.log(saying.size)
+
+
+// set 的操作
+
+const mySet = new Set()
+
+mySet.add('1')
+mySet.add('2')
+mySet.add('3')
+console.log(mySet.size)
+console.log(mySet.has('4'))
+
+for (let i of mySet) {
+    console.log(i)
+};
