7cea554d7f7a3d483c33e8f9500a8a1bfa42aa0b
anthropologistbiubiu <1319847957@qq.com>
2023-11-08 23:31:28 +0800

    modify

---
+++ basic/day08/day08.js
@@ -0,0 +1,27 @@
+// 使用类
+
+/*
+这些对应于类的三个关键特征：
+构造函数；
+实例方法和实例字段；
+静态方法和静态字段
+ */
+
+
+// 声明一个类
+
+class Myclass {
+
+    constructor();  // 构造函数
+    myField = "mycalss";
+    static myStaticMethod(){
+
+    };
+
+};
+
+// 在类体内有若干特性可以用
+
+
+
+
--- basic/day12/day12.js
+++ basic/day12/day12.js
@@ -6,3 +6,6 @@ var car = new Object()
 car.make = 'ford';
 car.year= '1969';
 
+for (var i in car) {
+   console.log(i,car[i]);
+};
