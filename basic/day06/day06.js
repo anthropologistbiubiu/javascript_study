106682798bc09904b0339b52f1fe2897b94c5f82
anthropologistbiubiu <1319847957@qq.com>
2023-11-13 23:33:45 +0800

    modify

--- basic/day06/day06.js
+++ basic/day06/day06.js
@@ -1,3 +1,22 @@
-// 异常处理
 
+// 用async 和await 来重写promise
+
+// task1
+function loadJson(url) {
+    return fetch(url)
+        .then(response => {
+            if (response.status == 200) {
+                return response.json();
+            } else {
+                throw new Error(response.status);
+            }
+        });
+}
+loadJson('https://javascript.info/no-such-user.json')
+    .catch(alert); // Error: 404
+
+
+// 异常处理
 // 11月13号开始完成异常处理的过程。
+
+
