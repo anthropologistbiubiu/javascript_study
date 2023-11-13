8d80dae1fccbf1545054093856e8ccf51b0e199a
anthropologistbiubiu <1319847957@qq.com>
2023-11-14 01:29:58 +0800

    modify

--- basic/day06/day06.js
+++ basic/day06/day06.js
@@ -2,6 +2,7 @@
 // 用async 和await 来重写promise
 
 // task1
+/*
 function loadJson(url) {
     return fetch(url)
         .then(response => {
@@ -12,9 +13,20 @@ function loadJson(url) {
             }
         });
 }
-loadJson('https://javascript.info/no-such-user.json')
-    .catch(alert); // Error: 404
 
+
+loadJson('https://javascript.info/no-such-user.json')
+        .catch(); // Error: 404
+*/
+async function LoadJson(url){
+   let response = await (url);
+   if (response.status == 200) {
+       return response.json();
+   }
+  return new Error(response.status)
+}
+
+console.log(LoadJson("www.baidu.com"))
 
 // 异常处理
 // 11月13号开始完成异常处理的过程。
