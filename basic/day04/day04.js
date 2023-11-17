68cc1d252bcb0c34e56f4c83265fd78f7947e1b1
anthropologistbiubiu <1319847957@qq.com>
2023-11-17 23:29:45 +0800

    modify

--- basic/day04/day04.js
+++ basic/day04/day04.js
@@ -1,6 +1,16 @@
 // 函数
 
 
-function myFunction(){
-
+function myFunction(theObject){
+    theObject.make = 'Toyota';
 };
+
+
+const mycar = {
+    make :'Honda',
+    model :'Accord',
+    year :1998,
+}
+
+myFunction(mycar);
+console.log(mycar.make)
