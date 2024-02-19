ad21daf47a0221a6e7abe79aee045847d38aea01
anthropologistbiubiu <1319847957@qq.com>
2024-02-19 21:58:00 +0800

    js

---
+++ dataStruct/Tree/binaryTree.js
@@ -0,0 +1,49 @@
+
+class Node {
+    constructor(data) {
+        this.data = data
+        this.left = null
+        this.right = null
+        this.height = 1
+    }
+}
+
+
+class AVLTree {
+   constructor() {
+       this.root = null
+   }
+   insert(data){
+       this.root = this._insert(data)
+   }
+   _insert(data){
+
+   }
+   _getHeight(root) {
+
+   }
+   _getBalanceFactory(){
+
+   }
+   _leftRotate(){
+
+   }
+   _rightRotate(){
+
+   }
+   _leftRightRotate(){
+
+
+   }
+   _rightLeftRotate(){
+
+   }
+
+
+
+
+}
+
+
+
+
