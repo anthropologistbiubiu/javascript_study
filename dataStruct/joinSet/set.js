d630e07a367cbddcee3321391323ae18bace4c63
anthropologistbiubiu <1319847957@qq.com>
2024-02-20 22:10:48 +0800

    joinSet

--- dataStruct/joinSet/set.js
+++ dataStruct/joinSet/set.js
@@ -3,7 +3,19 @@
 
 
 class DisJoinSet{
-    constructor() {
+    constructor(size) {
+        this.parent = new Array(size)
+        for (let i = 0; i < size; i++) {
+            this.parent[i] = i
+        }
+        this.rank = new Array(size).fill(0)
+    }
+
+    find(x){
+
+    }
+    union(x,y) {
+
     }
 
 }
