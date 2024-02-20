76bec88e647eaf05109c6858f354302bf7e78996
anthropologistbiubiu <1319847957@qq.com>
2024-02-20 23:08:26 +0800

    js

--- dataStruct/joinSet/set.js
+++ dataStruct/joinSet/set.js
@@ -14,10 +14,30 @@ class DisJoinSet{
         return this.parent[x]
     }
     union(x,y) {
-        xRoot = this.find(x)
-        yRoot = this.find(y)
-
+        let xRoot = this.find(x)
+        let yRoot = this.find(y)
+        if (xRoot === yRoot) {
+           return
+        }
+        if (this.rank[xRoot] < this.rank[yRoot]){
+           this.parent[xRoot] = yRoot
+        } else if (this.rank[yRoot] < this.rank[xRoot]) {
+            this.parent[yRoot] = xRoot
+        } else {
+            this.parent[yRoot] = xRoot
+            this.rank[xRoot]++
+        }
 
     }
 
 }
+
+
+ds1 = new DisJoinSet(10)
+ds1.union(0,2)
+ds1.union(1,2)
+ds1.union(3,4)
+console.log(ds1.find(1))
+console.log(ds1.find(2))
+console.log(ds1.find(3))
+console.log(ds1.find(4))
