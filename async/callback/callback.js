8227942c16b8120dc8a2625b4eea1f0fa6ea13c1
anthropologistbiubiu <1319847957@qq.com>
2024-03-03 20:12:30 +0800

    promise

--- async/callback/callback.js
+++ async/callback/callback.js
@@ -3,5 +3,20 @@
 
 function fetchData(callback){
 
-
+    setTimeout(function (){
+        console.log("开始执行")
+        callback()
+    },3000)
 }
+
+
+fetchData(function (){
+    console.log("获取数据中...")
+})
+
+
+
+
+
+
+
---
+++ async/promise/promise.js
@@ -0,0 +1,14 @@
+
+
+function fetchData(){
+
+
+    return new Promise((resolve,reject) =>{
+        setTimeout(function(){
+               resolve()
+        },5000)
+    })
+}
+
+
+
