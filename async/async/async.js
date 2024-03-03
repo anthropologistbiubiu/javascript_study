7d1a0605761b014acfc2f748ba3fffb51155db02
anthropologistbiubiu <1319847957@qq.com>
2024-03-03 21:09:42 +0800

    async

--- async/async/async.js
+++ async/async/async.js
@@ -1,3 +1,35 @@
 
 
 
+function fetchData(){
+  return new Promise((resolve,reject) => {
+      setTimeout(function(){
+          console.log('准备生成一个promise')
+          console.log('开始准备数据当中')
+          let flag = false
+          if (flag) {
+              let data = [1,2,3,4,5,6]
+              resolve(data)
+          } else {
+              let err = 'net work err'
+              reject(err)
+          }
+      },5000)
+  })
+}
+
+
+
+async function main(){
+    try {
+        const data = await fetchData();
+        console.log("promise 获取数据成功");
+        console.log(data);
+    } catch (err) {
+        console.log("promise 获取数据失败");
+        console.error(err);
+    }
+}
+
+
+main()
