8b876818de83f1e355fb7c64fdff447682c6c517
anthropologistbiubiu <1319847957@qq.com>
2024-03-03 20:40:31 +0800

    promise

--- async/promise/promise.js
+++ async/promise/promise.js
@@ -1,14 +1,28 @@
-
-
-function fetchData(){
-
-
-    return new Promise((resolve,reject) =>{
-        setTimeout(function(){
-               resolve()
-        },5000)
+function fetchData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            console.log('开始请求远程数据，生成一个promise')
+            let flag = false
+            if (flag){
+                resolve("rquest data successfully")
+            }else {
+               reject('net error')
+            }
+        }, 5000)
     })
 }
+
+
+fetchData().then(data => {
+    console.log("请求数据成功了")
+    console.log(data)
+}).catch(error => {
+    console.log("请求数据失败了")
+    console.log(error)
+})
+
+
+
 
 
 
