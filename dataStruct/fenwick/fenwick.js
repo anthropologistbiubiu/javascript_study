0624cebffee66e103cdc819d42dac9b060fb1019
anthropologistbiubiu <1319847957@qq.com>
2024-03-03 12:21:59 +0800

    fenwick

--- dataStruct/fenwick/fenwick.js
+++ dataStruct/fenwick/fenwick.js
@@ -1,38 +1,39 @@
-class FenwickTree {
-    constructor(size) {
-        this.size = size;
-        this.tree = new Array(size + 1).fill(0);
-    }
 
-    // 单点更新：在索引 index 处增加 value
-    update(index, value) {
-        while (index <= this.size) {
-            this.tree[index] += value;
-            index += this.lowbit(index);
+
+class FenwickTree{
+
+    constructor(nums) {
+        this.nums = nums
+        this.tree = new Array(nums.size()+1).fill(0)
+        this.capacity = nums.size()
+    }
+    Init() {
+        for (let i = 0; i < this.tree.length; i++) {
+            this.update(i+1,this.nums[i])
         }
+
     }
 
-    // 区间查询：计算数组从 1 到索引 index 的前缀和
-    query(index) {
-        let sum = 0;
-        while (index > 0) {
-            sum += this.tree[index];
-            index -= this.lowbit(index);
-        }
-        return sum;
+    update(i,delta){
+       while (i <= this.capacity) {
+           this.tree[i] += delta
+           i += this.lowBit(i)
+       }
     }
 
-    // 计算 x 的最低位 1 所代表的值
-    lowbit(x) {
-        return x & -x;
+    Query(index){
+
     }
+    lowBit(x){
+       return x & -x
+    }
+
+    QueryRange(start,end){
+    }
+
 }
 
-// 示例用法
-const nums = [1, 2, 3, 4, 5];
-const BIT = new FenwickTree(nums.length);
-for (let i = 0; i < nums.length; i++) {
-    BIT.update(i + 1, nums[i]); // 在索引 i+1 处增加 nums[i]
-}
-
-console.log(BIT.query(3)); // 查询前 3 个元素的和
+bit = new FenwickTree(nums)
+bit.Init()
+rangeResutl = bit.QueryRange(1,5)
+console.log(rangeResutl)
