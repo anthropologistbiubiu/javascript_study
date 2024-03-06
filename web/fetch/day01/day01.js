9ac55ffdeaab7612039dfd5bf3199959b699d557
anthropologistbiubiu <1319847957@qq.com>
2024-03-06 22:54:01 +0800

    fetch

--- web/fetch/day01/day01.js
+++ web/fetch/day01/day01.js
@@ -1,6 +1,6 @@
 
 
-fetch('https://api.example.com/data')
+fetch('http://localhost:9090/fetch')
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error('Error:', error));
