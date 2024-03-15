1c922b76f2a3ab501197627cfad7e89ac0be6f7d
anthropologistbiubiu <1319847957@qq.com>
2024-03-15 23:10:07 +0800

    quickSort

--- algorithm/sort/quickSort/quickSort.js
+++ algorithm/sort/quickSort/quickSort.js
@@ -1,19 +1,26 @@
-
-
-
 function quickSort(arr) {
+
     if (arr.length <= 1) {
-        return arr;
+        return arr
     }
-    const pivot = arr[0];
-    const left = [];
-    const right = [];
+    let left = [];
+    let right = [];
+    let pivot = arr[0]
     for (let i = 1; i < arr.length; i++) {
         if (arr[i] < pivot) {
-            left.push(arr[i]);
+            left.push(arr[i])
         } else {
-            right.push(arr[i]);
+            right.push(arr[i])
         }
     }
-    return quickSort(left).concat(pivot, quickSort(right));
+    return quickSort(left).concat(pivot,quickSort(right))
 }
+
+
+function main() {
+
+    arr = [9, 2, 1, 10, 22, 14, 7, 89, 15, 0];
+    console.log(quickSort(arr));
+};
+
+main();
