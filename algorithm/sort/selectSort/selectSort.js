c254aa45ccce97555b6fb5fde4552a0ab364cc65
anthropologistbiubiu <1319847957@qq.com>
2024-03-16 15:28:48 +0800

    fix issue

--- algorithm/sort/selectSort/selectSort.js
+++ algorithm/sort/selectSort/selectSort.js
@@ -5,20 +5,26 @@ function selectSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         minIndex = i;
         minValue = arr[i];
-        for (let j = minIndex + 1; j < arr.length; j++) {
+        for (let j = i + 1; j < arr.length; j++) {
             if (arr[j] < minValue) {
-                    minIndex =j;
-                    minValue = arr[j];
-            };
+                minIndex = j;
+                minValue = arr[j];
+            }
+            ;
         }
+        arr[minIndex] = arr[i];
         arr[i] = minValue;
-    };
+    }
+    ;
     return arr;
 };
 
 
-function main(){
+function main() {
 
     arr = [9, 2, 1, 10, 22, 14, 7, 89, 15, 0];
     console.log(selectSort(arr));
 };
+
+
+main();
