4fcf5e1d30333eab6b2866254b6fd33eeb2cbd59
anthropologistbiubiu <1319847957@qq.com>
2024-03-16 15:21:25 +0800

    selectSort

--- algorithm/sort/selectSort/selectSort.js
+++ algorithm/sort/selectSort/selectSort.js
@@ -1,10 +1,24 @@
+function selectSort(arr) {
 
-
-
-
-
-
-function selectSort(arr){
-
+    let minIndex = 0;
+    let minValue = 0;
+    for (let i = 0; i < arr.length; i++) {
+        minIndex = i;
+        minValue = arr[i];
+        for (let j = minIndex + 1; j < arr.length; j++) {
+            if (arr[j] < minValue) {
+                    minIndex =j;
+                    minValue = arr[j];
+            };
+        }
+        arr[i] = minValue;
+    };
     return arr;
-}
+};
+
+
+function main(){
+
+    arr = [9, 2, 1, 10, 22, 14, 7, 89, 15, 0];
+    console.log(selectSort(arr));
+};
