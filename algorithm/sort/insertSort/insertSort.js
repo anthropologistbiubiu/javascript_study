e5492db1cf63f03fa46024b9bc7db63c9beecb14
anthropologistbiubiu <1319847957@qq.com>
2024-03-16 16:41:13 +0800

    fix issue

--- algorithm/sort/insertSort/insertSort.js
+++ algorithm/sort/insertSort/insertSort.js
@@ -1,17 +1,13 @@
+function insertSort(arr) {
 
-
-
-
-function insertSort(arr){
-
-    for (let i = 1;i<arr.length;i++){
-        let j = i-1;
+    for (let i = 1; i < arr.length; i++) {
         let cur = arr[i];
-        while(j>=0&&arr[j]<cur){
-            arr[j+1] = cur[j]
-           j--;
+        let j = i - 1;
+        while (j >= 0 && arr[j] > cur) {
+            arr[j + 1] = arr[j];
+            j--;
         }
-        arr[j+1] = cur;
+        arr[j + 1] = cur;
     }
     return arr;
 };
