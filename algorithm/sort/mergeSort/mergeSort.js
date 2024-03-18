a8b81294a75922f940ea57dfcc4e5c2b0091d5a2
anthropologistbiubiu <1319847957@qq.com>
2024-03-18 23:11:47 +0800

    mergeSort

--- algorithm/sort/mergeSort/mergeSort.js
+++ algorithm/sort/mergeSort/mergeSort.js
@@ -4,13 +4,26 @@
 
 function mergeSort(arr){
 
+   if (arr.length<=1){
+      return arr;
+   };
+   let mid = math.floor(arr.length/2)
+   left = arr.slice(0,mid)
+   right = arr.slice(mid+1,arr.length);
+   mergeSort(left);
+   mergeSort(rightreturn mergeSort();
 
-   return arr;
-}
+};
 
 
 
 function merge(left,right){
 
-  return left.concat(right)
-}
+  return left.concat(right);
+
+};
+
+
+function main() {
+  return arr;
+};
