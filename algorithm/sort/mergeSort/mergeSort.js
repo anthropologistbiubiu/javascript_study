46f33558f95e7699667f54e29511a09df3cd19d9
anthropologistbiubiu <1319847957@qq.com>
2024-03-18 23:32:44 +0800

    mergeSort

--- algorithm/sort/mergeSort/mergeSort.js
+++ algorithm/sort/mergeSort/mergeSort.js
@@ -3,16 +3,31 @@
 
 
 function mergeSort(arr){
-
    if (arr.length<=1){
       return arr;
    };
    let mid = math.floor(arr.length/2)
    left = arr.slice(0,mid)
    right = arr.slice(mid+1,arr.length);
-   mergeSort(left);
-   mergeSort(rightreturn mergeSort();
+   let left = mergeSort(arr.slice(0,mid));
+   let right = mergeSort(arr.slice(mid+1,arr.length));
+   let l =0;
+   let r = 0;
+   let result = new Array();
+   while(l < left.length && right < right.length){
+      if (left[l] < right[r]){
+        resutl.push(left[l]);
+      } else {
+        result.push(right[r]);
+      };
+   }
+   if (l < left.length) {
 
+   }
+   if (r < right.length){
+
+   }
+   return result;
 };
 
 
@@ -20,7 +35,6 @@ function mergeSort(arr){
 function merge(left,right){
 
   return left.concat(right);
-
 };
 
 
