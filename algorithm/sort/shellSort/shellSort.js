d1292872a40a2ea0ed8b1e01a5f58c12c133b750
anthropologistbiubiu <1319847957@qq.com>
2024-03-20 01:30:35 +0800

    sort

--- algorithm/sort/shellSort/shellSort.js
+++ algorithm/sort/shellSort/shellSort.js
@@ -3,15 +3,19 @@
 function shellSort(arr) {
     for (let gap = Math.floor(arr.length / 2); gap > 0; gap = Math.floor(gap / 2)) {
         for (let i = gap; i < arr.length; i++) {
-            for (let j = i; j > gap && arr[j] > arr[i]; j -= gap) {
-                arr[j] = arr[j + gap];
-            }
+            let j;
+            for ( j = i; j >= gap && arr[j-gap] > arr[i]; j -= gap) {
+                console.log("j",j)
+                arr[j] = arr[j-gap];
+            };
             arr[j] = arr[i];
-        }
-    }
+        };
+    };
     return arr;
-}
+};
 
-let arr = [12, 34, 54, 2, 3];
+
+
+let arr = [12,34,54,2,3,0,19,24,38,89,229,74,1];
 console.log("Original Array: " + arr);
 console.log("Sorted Array: " + shellSort(arr));
