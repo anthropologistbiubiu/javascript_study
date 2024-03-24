ed4a7c414e7e4c069f62508aaa31df1fbaf005a8
anthropologistbiubiu <1319847957@qq.com>
2024-03-25 00:40:15 +0800

    heapSort

--- algorithm/sort/heapSort/heapSort.js
+++ algorithm/sort/heapSort/heapSort.js
@@ -2,63 +2,42 @@
 
 
 function MaxHeapSort(arr) {
-
-    len = arr.length;
-    function buildMaxHeap(arr, len) {
-        for (let i = Math.floor(len / 2); i >= 0; i--) {
-            let left = 2 * i + 1;
-            let right = 2 * i + 2;
-            let temp = arr[i];
-            let larget = i;
-            if (left < len && arr[left] > temp) {
-                temp = arr[left];
-                larget = left;
-            };
-            if (right < len && arr[right] > temp) {
-                larget = right;
-            };
-            if (i != larget) {
-                swap(i, larget)
-                AdjustDown(i, len);
-            };
-        };
-        console.log("heap",arr)
-    };
-
-    function AdjustDown(cur,length) {
-        let left = 2 * cur + 1;
-        let right = 2 * cur + 2;
-        let  next;
-        let tem = arr[cur];
-        while (left < length || right < length) {
-            if (arr[left] > tem) {
-               next = left;
-               tem = arr[left];
-            }
-            if (arr[right] > tem) {
-                next = right;
-                tem = arr[right];
-            }
-            left = 2 * next + 1;
-            right = 2 * next + 2;
-        };
-        swap(next,cur);
-    };
-
-    function swap(i, j) {
-        let tem = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tem;
-    };
-    buildMaxHeap(arr,len);
-    for (let i=0;i++;i< len){
-       swap(i,len-1);
-       console.log("xxxxx",arr)
-       AdjustDown(i,len-i-1);
+    buildMaxHeap(arr, arr.length);
+    for (let i = 0; i < arr.length; i++) {
+        swap(arr,0, arr.length - 1 - i);
+        AdjustDown(arr,0, arr.length - 1 - i);
     };
     return arr;
 };
 
-let arr = [12,34,54,2,3,0,19,24,38,89,229,74,1];
+function buildMaxHeap(arr, len) {
+    for (let i = Math.floor(len / 2); i >= 0; i--) {
+            AdjustDown(arr,i, len);
+    };
+};
+function AdjustDown(arr,cur, length) {
+    let j = cur;
+    let i = 2*j+1;
+    while(i < length){
+        if (i+1 < length && arr[i] < arr[i+1] ){
+           i++;
+        }
+        if (arr[i] > arr[j]) {
+           swap(arr,i,j)
+            j = i;
+            i = 2 * j + 1;
+        } else {
+            return;
+        }
+    };
+}
+
+function swap(arr,i, j) {
+    let tem = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tem;
+};
+
+let arr = [12, 34, 54, 2, 3, 0, 19, 24, 38, 89, 229, 74, 1];
 console.log("Array: " + arr);
 console.log("MaxHeapSort Array: " + MaxHeapSort(arr));
