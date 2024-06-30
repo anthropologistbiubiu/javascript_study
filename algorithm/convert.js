c0fb470b140261447e6f200556e94253e4444508
anthropologistbiubiu <1319847957@qq.com>
2024-06-30 17:22:57 +0800

    study

--- algorithm/convert.js
+++ algorithm/convert.js
@@ -8,5 +8,40 @@
  * @return {string}
  */
 var convert = function(s, numRows) {
+    
+    if (numRows == 1 || numRows > s.length){
+        return s
+    }
+    let rows = new Array(numRows)
+    let currentRow = 0
+    let goingDown = false
 
+    for (let char of s) {
+        rows[currentRow] += char
+        if (currentRow == 0 || currentRow == numRows - 1) {
+            goingDown = !goingDown
+        }
+        if (goingDown) {
+           currentRow += 1
+        } else {
+            currentRow -= 1
+        }
+    }
+    let result = strings.join(rows)
+    return result
 };
+
+/*
+示例 1：
+输入：s = "PAYPALISHIRING", numRows = 3
+输出："PAHNAPLSIIGYIR"
+
+
+示例 3：
+输入：s = "A", numRows = 1
+输出："A"
+
+ */
+
+console.log(convert("PAYPALISHIRING"))
+console.log(convert("A"))
