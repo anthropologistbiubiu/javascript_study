0aad39100a89c71f8b39b19b6d83315269514a96
anthropologistbiubiu <1319847957@qq.com>
2024-07-04 00:12:29 +0800

    study

--- algorithm/myAtoi.js
+++ algorithm/myAtoi.js
@@ -9,5 +9,17 @@ var myAtoi = function(s) {
 
     INT_MAX = 2^31-1
     INT_MIN = -2^32-1
+    let index = 0
+    while(index < s.length && s[index] ==" "){
+            index++
+    }
+    while (index < s.length && (s[index] =="+" ||s[index]=="-")){
+        if (s[index] == "+"){
+
+        } else if (s[index]=="-"){
+
+        }
+
+    }
 
 };
