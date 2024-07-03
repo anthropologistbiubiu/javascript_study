f44a1316ded1279720f33d3e01bb3c14b100daf3
anthropologistbiubiu <1319847957@qq.com>
2024-07-04 02:10:01 +0800

    study

--- algorithm/myAtoi.js
+++ algorithm/myAtoi.js
@@ -7,38 +7,28 @@
  */
 var myAtoi = function(s) {
 
-    INT_MAX = 2 ^ 31 - 1
-    INT_MIN = -2 ^ 32 - 1
+    const INT_MAX = Math.pow(2,31) - 1
+    const INT_MIN = -Math.pow(2,31)
     let index = 0
     let sign = 1
     let result = 0
     while (index < s.length && s[index] == " ") {
         index++
     }
-    while (index < s.length && (s[index] == "+" || s[index] == "-")) {
-        if (s[index] == "+") {
-            sign = 1
-        } else if (s[index] == "-") {
-            sign = -1
-        }
+    if (index < s.length && (s[index] == "+" || s[index] == "-")) {
+        sign = s[index] === "-" ? -1 : 1
         index++
     }
     while (index < s.length && s[index] >= "0" && s[index] <= "9") {
-        console.log('s[index]',s[index])
-        let pop = Number(s[index])
-        if (result > Math.floor(INT_MAX / 10) || (result == Math.floor(INT_MAX / 10) && pop > INT_MAX % 10 )) {
-            if (sign) {
-                return INT_MAX
-            } else {
-                return INT_MIN
-            }
+        let pop = parseInt(s[index],10)
+        if (result > Math.floor(INT_MAX / 10) || (result === Math.floor(INT_MAX / 10) && pop > (INT_MAX % 10 ))) {
+            return sign === 1 ? INT_MAX : INT_MIN
         }
         result = result * 10 + pop
-        console.log("result",result)
         index++
     }
     return result * sign
 };
 
 
-console.log(myAtoi("    123"))
+console.log(myAtoi("+-12"))
