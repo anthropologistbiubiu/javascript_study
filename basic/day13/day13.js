418878731329e02704b475d3ac5add96b78f3259
anthropologistbiubiu <1319847957@qq.com>
2024-07-14 23:26:19 +0800

    study

--- basic/day13/day13.js
+++ basic/day13/day13.js
@@ -1,2 +1,19 @@
 
 
+
+
+
+/*
+
+隐式返回
+ */
+const createArray = () => new Array(5).fill(0)
+
+
+// 显示返回
+let arr = new Array(3).fill(null).map(
+    () => { return new Array(3).fill(0)}
+)
+
+console.log(createArray())
+console.log(arr)
