a2283bc22e405cc8f100b344917cdb93d03d2b82
anthropologistbiubiu <62462764+anthropologistbiubiu@users.noreply.github.com>
2025-12-31 10:14:54 +0800

    Update fibGenerator.js

--- algorithm/fibGenerator.js
+++ algorithm/fibGenerator.js
@@ -1,1 +1,12 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    
+};
 
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
